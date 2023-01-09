d00c42ff5df9fd0562dfe460f6fbacf59df43c09
Tanner-Gladson <tannergladson1@gmail.com>
2023-01-09 12:04:20 -0500

    Finished testing StdAsset class

--- RealEstateTracker/src/App.java
+++ RealEstateTracker/src/App.java
@@ -3,14 +3,19 @@ import portfolioclasses.*;
 import static java.lang.System.out;
 import static java.util.Arrays.asList;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class App {
     
     public static void main(String[] args) throws Exception {
         test_std_asset_class();
+        
+        out.println("All tests complete!");
     }
     
-    public static void test_std_asset_class() {
-        
-    }
+    
+    
+    
+    
 }
