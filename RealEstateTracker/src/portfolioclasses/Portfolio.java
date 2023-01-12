015d034078d66a512efe54bc8008e3c8cd4867c5
Tanner-Gladson <tannergladson1@gmail.com>
2023-01-12 17:58:07 -0500

    Scaffoleded the portfolio classes

--- RealEstateTracker/src/portfolioclasses/Portfolio.java
+++ RealEstateTracker/src/portfolioclasses/Portfolio.java
@@ -1,5 +1,17 @@
 package portfolioclasses;
+import java.util.ArrayList;
+import assetclasses.*;
 
 public class Portfolio {
     
+    public ArrayList<BasicAsset> assets = new ArrayList<BasicAsset>();
+    
+    
+    public void add_asset(BasicAsset asset) {
+        assets.add(asset);
+    }
+    
+    public double get_init_equity_sum();
+    public double get_init_liability_sum();
+    public double get_init_asset_value_sum();
 }
