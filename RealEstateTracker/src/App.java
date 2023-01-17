6721d61ef48c8ad2cb295acb14cb10289f200941
Tanner-Gladson <tannergladson1@gmail.com>
2023-01-17 15:13:33 -0500

    Added source library

--- RealEstateTracker/src/App.java
+++ RealEstateTracker/src/App.java
@@ -15,17 +15,20 @@ public class App {
     
     public static void main(String[] args) throws Exception {        
         out.println("All tests complete!");
+        test_sim_2_xlsx();
     }
     
     
     public static void test_sim_2_xlsx() {
-        Portfolio port = new Portfolio("Port 1");
+        Portfolio port = new Portfolio("Portfolio 1");
+        Bond bond1 = new Bond("Bond 1", 100.0, 100.0, 1.0, .1);
+        Bond bond2 = new Bond("Bond 2", 5.0, 10.0, 1.0, .1);
+
+        port.add_asset(bond1);
+        port.add_asset(bond2);
         
-        Bond bond1 = new Bond("Bond 1", );
+        SimulatedPortfolio sim = new SimulatedPortfolio(port, 14);
         
-        
-        
-        
-        simulation2xlsx();
+        simulation2xlsx(sim);
     }
 }
--- RealEstateTracker/src/assetinterfaces/AssetUtilities.java
+++ RealEstateTracker/src/assetinterfaces/AssetUtilities.java
@@ -1,15 +1,27 @@
 package assetinterfaces;
 
 import java.io.FileInputStream;
+import org.apache.poi.xssf.*;
+
 
 public class AssetUtilities {
     
     public static void simulation2xlsx(SimulatedAsset sim) {
-        
+        FileInputStream file;
         try {
-            FileInputStream file = new FileInputStream("ExcelExports\test.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            
+            //XSSFSheet summary = workbook.createSheet();
+            
+            //XSSFRow revenues = summary.createRow(0);
+            //XSSFCell c =  revenues.createCell(0);
+            //c.setCellValue(true);
+            
+            
         } catch (Exception e) {
             System.out.println(e);
         }
+        
+        
     }
 }
