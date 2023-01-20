52389689256bed056371692db79f506a3f53c20c
Tanner-Gladson <tannergladson1@gmail.com>
2023-01-19 21:06:57 -0500

    Added notes for the implimentation of realestate

---
+++ RealEstateTracker/src/realestate/RealEstate.java
@@ -0,0 +1,39 @@
+package realestate;
+import assetinterfaces.*;
+
+public class RealEstate extends Asset {
+    
+    /* INITIAL DATA
+     * down_payment
+     * mortgage_liability
+     * purchase_price
+     * mortgage_maturity_months
+     * 
+     * closing_costs
+     * initial_repair_costs
+     * value_after_repair
+     * 
+     * 
+     * mortgage_mpr
+     * monthly_property_tax_rate
+     * 
+     * gross_rent
+     * vacancy_rate
+     * frac_manager_fee
+     * flat_manager_fee
+     * 
+     * maintenance
+     * capital_expenses
+     * utilities
+     * insurance
+     * HOA Fees
+     * 
+     * 
+     * TODO: Should these all be individual fields, or should you group
+     * them into frac/absolute ledgers?
+     */
+    
+    
+    // TRENDING DATA
+    
+}
---
+++ RealEstateTracker/src/realestate/SimulatedRealEstate.java
@@ -0,0 +1,21 @@
+package realestate;
+
+import assetinterfaces.SimulatedAsset;
+import assetinterfaces.*;
+
+public class SimulatedRealEstate extends SimulatedAsset {
+    RealEstate config;
+    
+    public SimulatedRealEstate(RealEstate config, int num_months) {
+        super(config);
+        this.config = config;
+        run_simulation(num_months);
+    }
+    
+    @Override
+    public void simulate_month() {
+        super.simulate_month();
+    }
+    
+    
+}
